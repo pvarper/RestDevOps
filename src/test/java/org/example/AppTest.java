283582910b59a8589d784259029fb1a167be123b
pvarper <p.varper@gmail.com>
2024-07-02 18:35:04 -0400

    primer commit local

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals(true, true);
+    }
+}
